674c33566386ee7198280678a0f0834ef6c170aa
Ubaid ur rehman <85988195+Ubaidsabir19@users.noreply.github.com>
2023-02-14 10:06:50 +0500

    Add files via upload

---
+++ 3.ts
@@ -0,0 +1,16 @@
+let myName = "Ubaid ur rehman";
+console.log("myName->"+myName);
+console.log("myName-> " + myName.toUpperCase());
+console.log("myName-> " + myName.toLowerCase());
+ console.log("title-> " + titleCase(myName));
+
+
+ function titleCase(str:String ){
+    var nArr = str.toLowerCase().split(' '); // how to convert string into an array
+    let final = [ ];
+     for(let  word of  nArr){
+        let nsring = word.charAt(0).toUpperCase()+ word.slice(1);
+       final.push(nsring);
+     }
+   return final.join(' ') // how to convert array into string..
+ };
